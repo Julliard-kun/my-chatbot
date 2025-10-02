a659fd79c2ab6b76c78d477c29fe5a288a07df96
Julliard Macatuggal <86839197+Julliard-kun@users.noreply.github.com>
2025-10-02 15:26:13 +0800

    Update .gitignore

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Get __dirname equivalent for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT;
 const HOSTNAME = process.env.HOSTNAME;
